8d361cd4149a6b6a31bfcdc9cdc710a50b3347c6
Luis Nieto <luizzitol@gmail.com>
2025-02-28 22:28:29 +0000

    fix path

--- ec2/config.sh
+++ ec2/config.sh
@@ -1,6 +1,11 @@
 # This script shoud take care of all the configuration needed for the EC2 instance
+SCRIPT_DIR=$(pwd)
+
 sudo yum update -y
-sudo yum install -y git docker
+sudo yum install -y git
+cd ~
+git clone https://github.com/luizzitol/habits.git
+
 
 # Docker stuff
 sudo yum install -y docker
@@ -12,6 +17,6 @@ sudo yum install libxcrypt-compat
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-sudo cp ./docker-compose.service /etc/systemd/system/docker-compose.service
+sudo cp ${SCRIPT_DIR}/docker-compose.service /etc/systemd/system/docker-compose.service
 sudo systemctl enable docker-compose
 sudo systemctl start docker-compose
