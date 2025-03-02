7b718187ebc980b79924a7866ec1bb45147e8cb6
Luis Nieto <luizzitol@gmail.com>
2025-03-02 13:36:27 +0000

    smplify settings

--- backend/api-server/core/settings.py
+++ backend/api-server/core/settings.py
@@ -58,7 +58,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "core.wsgi.application"
-ALLOWED_HOSTS = ["*"]
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -128,9 +127,28 @@ HEADLESS_FRONTEND_URLS = {
 }
 HEADLESS_SERVE_SPECIFICATION = True
 
+DEBUG = True
+ALLOWED_HOSTS = ["*"]
+
 if DEPLOYMENT_ENVIRONMENT == "prod":
-    from .settings_prod import *  # noqa
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_HOST = os.environ.get("EMAIL_HOST")
+    EMAIL_PORT = os.environ.get("EMAIL_PORT", 465)
+    EMAIL_USE_TLS = False
+    EMAIL_USE_SSL = True
+    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+    DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
+
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "habits",
+            "USER": os.environ.get("RDS_USERNAME"),
+            "PASSWORD": os.environ.get("RDS_PASSWORD"),
+            "HOST": os.environ.get("RDS_HOSTNAME"),
+            "PORT": os.environ.get("RDS_PORT", "5432"),
+        }
+    }
 else:
-    DEBUG = True
-    ALLOWED_HOSTS += ["localhost", "backend"]
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
